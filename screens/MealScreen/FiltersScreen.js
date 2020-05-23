1b154cce7569ace8249f509afc0cb314a2ea2319
Amine Hmidouche <amine.102.hmidouche@gmail.com>
2020-05-23 04:45:44 +0200

    Add Style Bottom Bar

---
+++ screens/MealScreen/FiltersScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View , Text } from 'react-native';
+
+const FiltersScreen = props =>{
+
+    return (
+<View><Text>Aminnnnnnnnnnnnnnnne</Text></View>
+
+    );
+
+}
+
+export default FiltersScreen;
